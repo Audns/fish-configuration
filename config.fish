d0bb2be3f6321529b8f7ce537d2e0f47f3bfd01d
audns <audns@tuta.io>
2025-07-04 20:37:20 +0800

    Initial commit

---
+++ config.fish
@@ -0,0 +1,45 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/audns/miniconda3/bin/conda
+    eval /home/audns/miniconda3/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/home/audns/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/audns/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /home/audns/miniconda3/bin $PATH
+    end
+end
+# <<< conda initialize <<<
+
+#starship init fish | source
+
+# fcitx is the default input method
+export XMODIFIERS=@im=fcitx
+#export GTK_IM_MODULE=fcitx
+#export QT_IM_MODULE=fcitx
+
+set -U fish_greeting ""
+alias cd='z'
+alias pparu='proxychains4 -q paru'
+alias vmrestart='sudo modprobe -v vmmon; sudo modprobe vmnet; sudo systemctl start vmware-networks.service; sudo systemctl start vmtoolsd.service'
+alias cat='bat'
+alias ls='eza --color=always --icons=always'
+alias lla='eza --color=always --icons=always -al'
+alias pcp='proxychains4 -q'
+alias ip='ip -c'
+
+# fz finder for dir
+function fz -d "Change directory using zoxide and fzf"
+    set -l dir (zoxide query --list | fzf --height 40% --layout reverse --border --query (commandline))
+    if test -n "$dir"
+        cd $dir
+    end
+    commandline -f repaint
+end
+
+# Auto-launch tmux if not already in a tmux session and in an interactive terminal
+if command -q tmux
+    and status is-interactive
+    and not set -q TMUX
+    exec tmux
+end
